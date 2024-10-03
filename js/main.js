2ac20b71beb3bbb68ba52062225801d720688db8
BrandenStudentDev <brandengrywinski@gmail.com>
2024-10-02 21:14:07 -0600

    finished lab3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 // Enter your code below.
+const jsResources = document.querySelector(".javascript-resources");
+const showResources = document.querySelector("#show-resources");
+
+showResources.addEventListener('click',(event)=>{
+    jsResources.classList.remove("d-none");
+});
+
+jsResources.addEventListener('mouseover',(event)=>{
+    event.target.classList.add("fw-bold");
+});
+
+jsResources.addEventListener('mouseout',(event)=>{
+    event.target.classList.remove("fw-bold");
+});
+
+jsResources.addEventListener('click',(event)=>{
+    event.target.classList.add("fst-italic");
+});
